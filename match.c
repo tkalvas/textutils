a826c2e2f7fe9e18ad8095f479ecbf470d6d75db
Teemu Kalvas <teemu@montel.fi>
2020-01-31 13:47:10 +0200

    fix matcher, cleanup code

--- match.c
+++ match.c
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int color = 0;
+int use_color = 0;
 
 char *foreground_red = "\033[31m";
 char *foreground_green = "\033[32m";
@@ -16,59 +16,75 @@ char *foreground_reset = "\033[39m";
 char *attribute_reset = "\033[0m";
 char *bold = "\033[1m";
 
+void color_vprintf(char *color, char *fmt, va_list ap) {
+  if (use_color) fprintf(stderr, "%s", color);
+  vfprintf(stderr, fmt, ap);
+  if (use_color) fprintf(stderr, "%s", foreground_reset);
+}
+
+void err_vprintf(char *fmt, va_list ap) {
+  color_vprintf(foreground_red, fmt, ap);
+}
+
+void info_vprintf(char *fmt, va_list ap) {
+  color_vprintf(foreground_green, fmt, ap);
+}
+
 void err_printf(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  if (color)
-    fprintf(stderr, "%s", foreground_red);
-  vfprintf(stderr, fmt, ap);
-  if (color)
-    fprintf(stderr, "%s", foreground_reset);
+  err_vprintf(fmt, ap);
   va_end(ap);
 }
 
 void info_printf(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  if (color)
-    fprintf(stderr, "%s", foreground_green);
-  vfprintf(stderr, fmt, ap);
-  if (color)
-    fprintf(stderr, "%s", foreground_reset);
+  info_vprintf(fmt, ap);
   va_end(ap);
+}
+
+void errno_printf(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  err_vprintf(fmt, ap);
+  va_end(ap);
+  err_printf(": %s (%d)\n", strerror(errno), errno);
+  exit(errno);
+}
+
+void exit_printf(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  err_vprintf(fmt, ap);
+  va_end(ap);
+  exit(1);
 }
 
 long str2long(char *str) {
   char *end;
   long res = strtol(str, &end, 10);
-  if (!*str || *end) {
-    err_printf("cannot convert \"%s\" to long\n", str);
-    exit(1);
-  }
+  if (!*str || *end)
+    exit_printf("cannot convert \"%s\" to long\n", str);
   return res;
 }
 
 int file4read(char *name) {
   int fd = open(name, O_RDONLY);
-  if (fd == -1) {
-    err_printf("cannot open file \"%s\": %s (%d)\n",
-               name, strerror(errno), errno);
-    exit(errno);
-  }
+  if (fd == -1)
+    errno_printf("cannot open file \"%s\"", name);
   return fd;
 }
 
 void *allocate(int size) {
   char *res = malloc(size);
-  if (!res) {
-    err_printf("cannot allocate %d bytes\n", size);
-    exit(1);
-  }
+  if (!res)
+    exit_printf("cannot allocate %d bytes\n", size);
   return res;
 }
 
 long max_columns = 65536L;
-int count = 0;
+int report_count = 0;
 
 void parse_options(int argc, char **argv) {
   while (1) {
@@ -84,13 +100,13 @@ void parse_options(int argc, char **argv) {
     if (c == -1) break;
     switch (c) {
     case 'c':
-      count = 1;
+      report_count = 1;
       break;
     case 'm':
       max_columns = str2long(optarg);
       break;
     case 'r':
-      color = 1;
+      use_color = 1;
       break;
     }
   }
@@ -103,32 +119,48 @@ char *buffer;
 int buffer_pos = 0;
 int buffer_len;
 
-int binary = 0;
+int state_binary = 0;
 
-void output_match(char *line, int len, int pos) {
-  if (count) return;
-  if (pos > 0)
-    fwrite(line, 1, pos, stdout);
-  if (color)
-    printf("%s", bold);
-  fwrite(line + pos, 1, match_param_len, stdout);
-  if (color)
-    printf("%s", attribute_reset);
-  if (pos + match_param_len < len)
-    fwrite(line + pos + match_param_len, 1, len - (pos + match_param_len),
-           stdout);
+void output_part(char *start, char *ptr) {
+  if (report_count || !use_color) return;
+  if (ptr > start)
+    fwrite(start, 1, ptr - start, stdout);
+  printf("%s", bold);
+  fwrite(ptr, 1, match_param_len, stdout);
+  printf("%s", attribute_reset);
 }
 
-int consume_line(char *line, int len) {
-  if (len < match_param_len) return 0;
-  char *ptr = memchr(line, match_param[0], len);
-  if (ptr && ptr - line + match_param_len <= len) {
+void output_tail(char *start, int len) {
+  if (report_count || !use_color) return;
+  fwrite(start, 1, len, stdout);
+}
+
+void output_full(char *line, int len) {
+  if (report_count || use_color) return;
+  fwrite(line, 1, len, stdout);
+}
+
+int consume_line(char *line, int line_len) {
+  int match = 0;
+  char *prev = line;
+  char *start = line;
+  int len = line_len;
+  while (len >= match_param_len) {
+    char *ptr = memchr(start, match_param[0], len - match_param_len + 1);
+    if (!ptr) break;
     if (!memcmp(ptr, match_param, match_param_len)) {
-      output_match(line, len, ptr - line);
-      return 1;
+      output_part(prev, ptr);
+      match += 1;
+      prev = start = ptr + match_param_len;
+      len = line_len - (start - line);
+    } else {
+      start = ptr + 1;
+      len = line_len - (start - line);
     }
   }
-  return 0;
+  if (match) output_full(line, line_len);
+  if (match) output_tail(prev, line_len - (prev - line));
+  return match;
 }
 
 int consume_binary() {
@@ -147,7 +179,7 @@ int consume_binary() {
 }
 
 int consume(int force) {
-  if (binary) {
+  if (state_binary) {
     return consume_binary();
   }
   char *ptr = memchr(buffer, '\n', buffer_pos);
@@ -159,42 +191,34 @@ int consume(int force) {
     return match;
   }
   if (force) {
-    binary = 1;
+    state_binary = 1;
     return consume_binary();
   }
   return 0;
 }
 
-int global_match = 0;
+int match_count = 0;
 
 void run_fd(int fd) {
   while (1) {
     int len = read(fd, buffer + buffer_pos, buffer_len - buffer_pos);
-    if (len == -1) {
-      err_printf("cannot read: %s (%d)\n", strerror(errno), errno);
-      exit(errno);
-    }
+    if (len == -1)
+      errno_printf("cannot read");
     if (!len) return;
     buffer_pos += len;
-    global_match += consume(buffer_pos == buffer_len);
+    match_count += consume(buffer_pos == buffer_len);
   }
 }
 
 void run(int index, int argc, char **argv) {
-  if (index == argc) {
-    err_printf("no match parameter\n");
-    exit(1);
-  }
+  if (index == argc)
+    exit_printf("no match parameter\n");
   match_param = argv[index++];
   match_param_len = strlen(match_param);
-  if (!match_param_len) {
-    err_printf("match parameter empty\n");
-    exit(1);
-  }
-  if (match_param_len >= max_columns) {
-    err_printf("match parameter not less than maximum line length\n");
-    exit(1);
-  }
+  if (!match_param_len)
+    exit_printf("match parameter empty\n");
+  if (match_param_len >= max_columns)
+    exit_printf("match parameter not less than maximum line length\n");
   buffer_len = max_columns;
   buffer = allocate(buffer_len);
   if (index == argc) {
@@ -211,11 +235,11 @@ void run(int index, int argc, char **argv) {
 int main(int argc, char **argv) {
   parse_options(argc, argv);
   run(optind, argc, argv);
-  if (binary && global_match) {
+  if (state_binary && match_count) {
     info_printf("binary file matches\n");
   }
-  if (!binary && count) {
-    info_printf("%d matches\n", global_match);
+  if (!state_binary && report_count) {
+    info_printf("%d matches\n", match_count);
   }
-  return !global_match;
+  return !match_count;
 }
